289885f1c141c1bd5eaa32e0798f79233933ecb2
GK <gabykassab0@gmail.com>
2024-12-18 19:59:27 +0200

    initzializing the serializers file

---
+++ core/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Users
+
+class Userserializer(ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['id','email','password']
