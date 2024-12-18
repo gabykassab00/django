b28595c81314c9eec658138dc5fc0ae945a82116
GK <gabykassab0@gmail.com>
2024-12-18 18:19:06 +0200

    testing regitser api

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class Registerapiview(APIView):
     def post(self,request):
-        pass
+        return Response('hello')
     
