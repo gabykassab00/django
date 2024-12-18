fc592d6f0d50d353c240dc03c7dbbdeeec4e2680
GK <gabykassab0@gmail.com>
2024-12-18 17:33:48 +0200

    migrating  users table datbase

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-class User(AbstractUser):
+class Users(AbstractUser):
     email = models.CharField(max_length=255,unique=True)
     password = models.CharField(max_length=255)
     
