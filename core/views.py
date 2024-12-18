2af5f9578047c636153089dfa0ccb46099752ff1
GK <gabykassab0@gmail.com>
2024-12-18 20:02:32 +0200

    checking if password and confirm password matches

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import exceptions
 class Registerapiview(APIView):
     def post(self,request):
+        data = request.data 
+        
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('passwords do not match')
+        
+        
+        
         return Response(request.data)
     
