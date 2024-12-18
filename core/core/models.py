17712741b9b7885de661bf72f270137e36638213
GK <gabykassab0@gmail.com>
2024-12-18 17:24:35 +0200

    creating users table datbase

--- core/core/models.py
+++ core/core/models.py
@@ -1,4 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 class User(AbstractUser):
-    pass
-
+    email = models.CharField(max_length=255,unique=True)
+    password = models.CharField(max_length=255)
+    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
