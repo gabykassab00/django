46b1fb38237e20c9b53fef3654d4fc0061c96715
GK <gabykassab0@gmail.com>
2024-12-18 22:44:34 +0200

    saving user

--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import exceptions
+from .serializers import Userserializer
+
+
+
 class Registerapiview(APIView):
     def post(self,request):
         data = request.data 
@@ -9,7 +13,8 @@ class Registerapiview(APIView):
         if data['password'] != data['password_confirm']:
             raise exceptions.APIException('passwords do not match')
         
-        
-        
-        return Response(request.data)
+        serializer = Userserializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
     
