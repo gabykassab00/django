34528ae0e364b026be1c5e08634c8c745ff58aca
GK <gabykassab0@gmail.com>
2024-12-19 18:59:04 +0200

    backend : creating a decoder for the token

--- core/authentication.py
+++ core/authentication.py
@@ -30,8 +30,7 @@ def decode_access_token(token):
     try:
         payload = jwt.decode(token,'access_secret',algorithms='HS256')
         return payload['user_id']
-    except Exception as e :
-        print(e)
+    except  :
         raise exceptions.AuthenticationFailed('unauthenticated')
      
 def create_refresh_token(id):
@@ -40,5 +39,13 @@ def create_refresh_token(id):
         'exp':datetime.datetime.utcnow()+datetime.timedelta(days=7),
         'iat':datetime.datetime.utcnow()
     },'refresh_token',algorithm='HS256')
+    
+    
+def decode_refresh_token(token):
+    try:
+        payload = jwt.decode(token,'refresh_secret',algorithms='HS256')
+        return payload['user_id']
+    except :
+        raise exceptions.AuthenticationFailed('unauthenticated')
      
      
