048ea4d44a1399b59325b05bde7b1c2dcf37fc2e
GK <gabykassab0@gmail.com>
2024-12-19 13:42:18 +0200

    generate refresh  token

--- core/authentication.py
+++ core/authentication.py
@@ -8,3 +8,10 @@ def create_access_token(id):
          'iat':datetime.datetime.utcnow()
      },'access_secret',algorithm='HS256')
      
+def create_refresh_token(id):
+    return jwt.encode({
+        'user_id':id,
+        'exp':datetime.datetime.utcnow()+datetime.timedelta(days=7),
+        'iat':datetime.datetime.utcnow()
+    },'refresh_token',algorithm='HS256')
+     
