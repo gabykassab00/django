1a66227586552decccc87560bc90e8378b02bb83
GK <gabykassab0@gmail.com>
2024-12-19 12:55:29 +0200

    hashing password

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 class Users(AbstractUser):
+    username=None
     email = models.CharField(max_length=255,unique=True)
     password = models.CharField(max_length=255)
     
