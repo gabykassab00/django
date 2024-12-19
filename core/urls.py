c26cf6ad809d3b2749ebf34535aa41db71abf7de
GK <gabykassab0@gmail.com>
2024-12-19 21:31:53 +0200

    backend : logout url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import Registerapiview,Loginapiview,Userapiview,Refreshapiview
+from .views import Registerapiview,Loginapiview,Userapiview,Refreshapiview,Logoutapiview
 urlpatterns = [
     path('register',Registerapiview.as_view()),
     path('login',Loginapiview.as_view()),
     path('user',Userapiview.as_view()),
     path('refresh',Refreshapiview.as_view()),
+    path('logout',Logoutapiview.as_view()),
 
 ]
