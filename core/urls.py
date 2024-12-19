f93612b66001bfefa8b915601d994270249e745a
GK <gabykassab0@gmail.com>
2024-12-19 13:04:10 +0200

    adding login api to urls

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import Registerapiview
+from .views import Registerapiview,Loginapiview
 urlpatterns = [
-    path('register',Registerapiview.as_view())
+    path('register',Registerapiview.as_view()),
+    path('login',Loginapiview.as_view())
 ]
