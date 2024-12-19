8a892ecc86820c3e1432701c7bfbec9b40b7756d
GK <gabykassab0@gmail.com>
2024-12-19 16:00:35 +0200

    backend : checking id based on token

--- core/authentication.py
+++ core/authentication.py
@@ -1,5 +1,5 @@
 import jwt,datetime
-
+from rest_framework import exceptions
 
 def create_access_token(id):
      return jwt.encode({
@@ -8,10 +8,18 @@ def create_access_token(id):
          'iat':datetime.datetime.utcnow()
      },'access_secret',algorithm='HS256')
      
+def decode_access_token(token):
+    try:
+        payload = jwt.decode(token,'access_secret',algorithms='HS256')
+        return payload.user_id
+    except:
+        raise exceptions.AuthenticationFailed('unauthenticated')
+     
 def create_refresh_token(id):
     return jwt.encode({
         'user_id':id,
         'exp':datetime.datetime.utcnow()+datetime.timedelta(days=7),
         'iat':datetime.datetime.utcnow()
     },'refresh_token',algorithm='HS256')
+     
      
