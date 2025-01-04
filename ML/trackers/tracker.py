90134f35133b0103bf6a5ada958bd33bb904217c
GK <gabykassab0@gmail.com>
2025-01-05 00:25:50 +0200

    backend : returning frames in draw annotations

--- ML/trackers/tracker.py
+++ ML/trackers/tracker.py
@@ -118,3 +118,7 @@ class Tracker:
             for track_id , player in player_dict.items():
                 frame=self.draw_ellipse(frame,player["bbox"],(0,0,255),track_id)
                 
+            output_video_frames.append(frame)
+            
+        return output_video_frames
+                
