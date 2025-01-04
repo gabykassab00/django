94e47797c8b6549265c7112f4168583a67cd70ee
GK <gabykassab0@gmail.com>
2025-01-04 22:41:43 +0200

    backend : defined the constructor

--- ML/trackers/tracker.py
+++ ML/trackers/tracker.py
@@ -1,5 +1,8 @@
+from ultralytics import YOLO
+
 class Tracker:
-    def __init__(self):
-        pass
+    def __init__(self,model_path):
+        self.model = YOLO(model_path)
     
-    
+    def get_object_tracks(self,frames):
+        
