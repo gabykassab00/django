83abb94b42ccc0f5007424547f6621bbf4e55c4c
GK <gabykassab0@gmail.com>
2025-01-04 15:36:06 +0200

    backend : read video and saving it in main

--- ML/main.py
+++ ML/main.py
@@ -1,7 +1,11 @@
 from utils._init_ import read_video,save_video
 
 def main():
-    print("hello,world!")
+    #read video
+    video_frames = read_video('input_videos/input_videos/v.mp4')
+    
+    #save video
+    save_video(video_frames,'output_videos/output_video.avi')
     
 if __name__ == "_main_":
     main()
