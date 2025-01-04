2806766c7322156c3fcc7bdb53fd6c27ef9f8d6b
GK <gabykassab0@gmail.com>
2025-01-04 22:46:33 +0200

    backend : defining the detect frame function

--- ML/trackers/tracker.py
+++ ML/trackers/tracker.py
@@ -3,6 +3,15 @@ from ultralytics import YOLO
 class Tracker:
     def __init__(self,model_path):
         self.model = YOLO(model_path)
+        
+    def detect_frames(self,frames):
+       batch_size = 20 
+       detections =[]
+       
+       detections =  self.model.predict(frames)
+       
     
     def get_object_tracks(self,frames):
+        
+        detections
         
