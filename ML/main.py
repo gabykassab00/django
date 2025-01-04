ba12f93b047ea0b31900ac5c38767256d75ce35a
GK <gabykassab0@gmail.com>
2025-01-04 22:22:05 +0200

    backend : fixes

--- ML/main.py
+++ ML/main.py
@@ -2,10 +2,10 @@ from utils._init_ import read_video,save_video
 
 def main():
     #read video
-    video_frames = read_video('input_videos/input_videos/v.mp4')
+    video_frames = read_video('ML/input_videos/v.mp4')
     
     #save video
-    save_video(video_frames,'output_videos/output_video.avi')
+    save_video(video_frames,'ML/output_videos/video_result.avi')
     
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
