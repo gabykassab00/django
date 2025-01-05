7ae26be5b1a519eaf6c42f9410d65edba76eb575
GK <gabykassab0@gmail.com>
2025-01-05 13:22:19 +0200

    backend : getting the clustering model

--- ML/team_assigner/team_assigner.py
+++ ML/team_assigner/team_assigner.py
@@ -7,6 +7,10 @@ class Teamassigner:
         image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
         
         top_half_image = image[0:int(image.shape[0]/2),:]
+        
+        #get clustering model 
+        
+        kmeans = self.get_clustering_model(top_half_image)
     
     
     def assign_team_color(self,frame,player_detections):
