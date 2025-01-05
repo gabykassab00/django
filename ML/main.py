d291fb03a4d651a2c081a7a3a2f848d749b93ec0
GK <gabykassab0@gmail.com>
2025-01-05 13:47:48 +0200

    backend : looping in all frames to track players

--- ML/main.py
+++ ML/main.py
@@ -19,7 +19,9 @@ def main():
     team_assigner = Teamassigner()
     team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
     
-    
+    for frame_num , player_track in enumerate(tracks['players']):
+        for player_id , track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],track['bbox'],player_id)
     
     
     
