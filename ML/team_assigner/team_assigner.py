b2a6b8fc337e7fc1a2a4db3c43424bd29e598c98
GK <gabykassab0@gmail.com>
2025-01-05 13:21:40 +0200

    backend : getting the player color

--- ML/team_assigner/team_assigner.py
+++ ML/team_assigner/team_assigner.py
@@ -3,6 +3,12 @@ class Teamassigner:
         pass 
     
     
+    def get_player_color(self,frame,bbox):
+        image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+        
+        top_half_image = image[0:int(image.shape[0]/2),:]
+    
+    
     def assign_team_color(self,frame,player_detections):
         player_colors = []
         for _,player_detection in player_detections.items():
