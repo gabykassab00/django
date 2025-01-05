beadeb3377a2bda389e53b715446066fbba70e0a
GK <gabykassab0@gmail.com>
2025-01-05 22:05:23 +0200

    bakcend : getting corner frames

--- ML/camera_movement_estimator/camera_movement_estimator.py
+++ ML/camera_movement_estimator/camera_movement_estimator.py
@@ -1,8 +1,13 @@
 import pickle
-
+import cv2
 class Cameramovement():
-    def __init__(self):
+    def __init__(self,frame):
         pass
     
     def get_camera_movement(self,frames,read_from_stub=False,stub_path=None):
+        #read from stub 
         
+        camera_movement = [[0,0]*len(frames)]
+        
+        old_gray = cv2.cvtColor(frames[0],cv2.COLOR_BGR2GRAY)
+        old_features = cv2.goodFeaturesToTrack(old_gray,)
