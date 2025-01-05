7a250129d9da03d198fb763469b96b1ccb9d1ccd
GK <gabykassab0@gmail.com>
2025-01-05 15:28:41 +0200

    backend : created the assign ball to player function

--- ML/player_ball_assigner/player_ball_assigner.py
+++ ML/player_ball_assigner/player_ball_assigner.py
@@ -2,3 +2,9 @@ import sys
 sys.path.append("../")
 from utils.bbox_utils import get_center_of_bbox 
 
+class Playerassigner():
+    def __init__(self):
+        self.max_player_ball_distance = 70 
+        
+    def assign_ball_to_player(self,players,ball_bbox):
+        
