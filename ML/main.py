9f0e94fa5f8cf2a416a52d64e67ee1173778ac29
GK <gabykassab0@gmail.com>
2025-01-05 20:12:53 +0200

    backend :  adding frames for team1 and team2

--- ML/main.py
+++ ML/main.py
@@ -39,7 +39,7 @@ def main():
     #assign ball aquisition 
     
     player_assigner = Playerassigner()
-    team_ball_control =[]
+    team_ball_control =[-1]
     for frame_num , player_track in enumerate(tracks['players']):
         ball_bbox = tracks['ball'][frame_num][1]['bbox']
         assigned_player = player_assigner.assign_ball_to_player(player_track,ball_bbox)
@@ -50,7 +50,7 @@ def main():
             
         else :
             team_ball_control.append(team_ball_control[-1])
-    
+    team_ball_control = np.array(team_ball_control)
 
     
     
--- ML/trackers/tracker.py
+++ ML/trackers/tracker.py
@@ -164,8 +164,14 @@ class Tracker:
         #get the number of time each team had the ball
         
         team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
         
+        cv2.putText(frame,f"team 1 ball control :{team_1*100:.2f}%",(1400,900),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
+        cv2.putText(frame,f"team 2 ball control :{team_2*100:.2f}%",(1400,950),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
         
+        return frame
         
     def draw_annotations(self,video_frames,tracks,team_ball_control):
         output_video_frames = []
