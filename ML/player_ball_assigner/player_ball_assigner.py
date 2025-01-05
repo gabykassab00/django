885422f2f962be8a5f0cb5a7f743671cc506c249
GK <gabykassab0@gmail.com>
2025-01-05 15:30:26 +0200

    backend : looping in all players

--- ML/player_ball_assigner/player_ball_assigner.py
+++ ML/player_ball_assigner/player_ball_assigner.py
@@ -7,4 +7,7 @@ class Playerassigner():
         self.max_player_ball_distance = 70 
         
     def assign_ball_to_player(self,players,ball_bbox):
+        ball_position = get_center_of_bbox(ball_bbox)
         
+        for player_id , player in players.items():
+            player_bbox = player["bbox"]
