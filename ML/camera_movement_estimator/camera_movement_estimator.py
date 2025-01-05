d1c3081b3b98f9400516f0195784444331e51486
GK <gabykassab0@gmail.com>
2025-01-05 22:08:43 +0200

    bakcend : getting top and bottom features

--- ML/camera_movement_estimator/camera_movement_estimator.py
+++ ML/camera_movement_estimator/camera_movement_estimator.py
@@ -1,8 +1,12 @@
 import pickle
 import cv2
+import numpy as np
 class Cameramovement():
     def __init__(self,frame):
-        pass
+        first_frame_grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        mask_features = np.zeros_like(first_frame_grayscale)
+        mask_features[:,0:20] = 1 
+        mask_features[:,900:1050] = 1 
     
     def get_camera_movement(self,frames,read_from_stub=False,stub_path=None):
         #read from stub 
