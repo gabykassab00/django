961a6cdbd8b89d6447d6400feba15783048966d6
GK <gabykassab0@gmail.com>
2025-01-06 14:04:49 +0200

    backend : adding frame window every loop

--- ML/speed_and_distance_estimator/speed_and_distance_estimator.py
+++ ML/speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -8,3 +8,6 @@ class Speedanddistanceestimator():
         for object , object_tracks in tracks.items():
             if object == 'ball' or object == 'referees' :
                 continue
+            number_of_frames = len(object_tracks)
+            for frame_num in range(0,number_of_frames,self.frame_window):
+                
