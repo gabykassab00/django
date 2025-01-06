1ae7d9df88a934d88241d75c1a22303760a97bf0
GK <gabykassab0@gmail.com>
2025-01-06 14:01:35 +0200

    backend : added speed and distance to track function

--- ML/speed_and_distance_estimator/speed_and_distance_estimator.py
+++ ML/speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -1,2 +1,8 @@
 class Speedanddistanceestimator():
     def __init__(self):
+        self.frame_window = 5 
+        self.frame_rate = 24 
+        
+        
+    def add_speed_and_distance_to_tracks(self,tracks):
+        
